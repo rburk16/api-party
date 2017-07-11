0344330da2108ef0c65ab7de23c9ddbd2ee2a280
Ryan Burkholder <rcburkholder@anderson.edu>
2017-07-11 15:05:35 -0400

    Add React Router, header content, NavLink and Route for Github, and style header and NavLinks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="App-headings">
+            <h3>Ain't no like party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+              <li>
+                <NavLink to="/github">Github API</NavLink>
+              </li>
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Switch>
+            <Route path='/github' render={() => <h1>Github!</h1>} />
+            <Route render={() => <p>To get started, click one of the links above</p>} />
+          </Switch>
       </div>
     );
   }
