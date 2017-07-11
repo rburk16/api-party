b2b5080330c0b1b3fe43d7490eddae1cbe3d0f35
Ryan Burkholder <rcburkholder@anderson.edu>
2017-07-11 15:39:19 -0400

    Create Github user component

---
+++ src/GithubUser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import './GithubUser.css'
+
+class GithubUser extends Component {
+    render() {
+        return <h1>HELIO</h1>
+    }
+}
+
+export default GithubUser
