4b608946bff9cf00c07bc9bca6769ee66a36355c
Ryan Burkholder <rcburkholder@anderson.edu>
2017-07-11 16:15:36 -0400

    Perform fetch request to get user data from Github API and add markup and styling for the user and update user info when location changes

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -3,8 +3,41 @@ import { Route } from 'react-router-dom'
 import './GithubUser.css'
 
 class GithubUser extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            user: {}
+        }
+
+        this.fetchUserData(props)
+    }
+
+    fetchUserData = (props) => {
+        fetch(`https://api.github.com/users/${props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user }))
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const locationChanged = nextProps.location !== this.props.location
+        if (locationChanged) {
+            this.fetchUserData(nextProps)
+        }
+    }
+
     render() {
-        return <h1>HELIO</h1>
+        const { user } = this.state
+        return (
+            <div className="github-user">
+                <img src={user.avatar_url} alt="github user avatar" />
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+            </div>
+        )
     }
 }
 
